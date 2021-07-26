b7a0f7574587e05ff1ec6035701f3dfa209fbb99
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-07-26 08:07:39 -0600

    Create SelectMetrics.tsx in Features/SelectMetrics, added likely imports

---
+++ src/Features/SelectMetrics/SelectMetrics.tsx
@@ -0,0 +1,6 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { actions } from './reducer';
+import { Provider, createClient, useQuery } from 'urql';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { IState } from '../../store';
